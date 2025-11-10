c6f32cc6b976646f65168f72434ced4541fe111f
Sriram Nagarajan <132087071+Sriram-2706@users.noreply.github.com>
2025-11-10 08:58:53 +0530

    renegae

--- app/app.py
+++ app/app.py
@@ -65,5 +65,7 @@ def startup_db_init():
 if __name__ == '__main__':
     with app.app_context():
         startup_db_init()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=False)
+
