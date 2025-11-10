860a2ae1658b1ba4c07fef00770c2874c3290ea4
Sriram Nagarajan <132087071+Sriram-2706@users.noreply.github.com>
2025-11-10 09:03:39 +0530

    Change default port from 5000 to 10000

--- app/app.py
+++ app/app.py
@@ -66,6 +66,6 @@ if __name__ == '__main__':
     with app.app_context():
         startup_db_init()
     import os
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 10000))  
     app.run(host='0.0.0.0', port=port, debug=False)
 
